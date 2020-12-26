9b7c3dd77f8a48b1bcb8beb036cdff6efee040cb
Eugene Gershnik <gershnik@hotmail.com>
2020-12-25 20:30:41 -0800

    Adding sample

---
+++ sample/main.mm
@@ -0,0 +1,98 @@
+#import <Foundation/Foundation.h>
+
+#include "../include/BlockUtil.h"
+#include "../include/NSStringUtil.h"
+#include "../include/NSObjectUtil.h"
+#include "../include/NSNumberUtil.h"
+
+#include <iostream>
+#include <unordered_map>
+#include <map>
+
+@interface BlockDemo : NSObject
+
+@end
+
+@implementation BlockDemo
+
+- (void) callBlock {
+    dispatch_sync(dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0), makeBlock([weakSelf = makeWeak(self)]() {
+        
+        auto self = makeStrong(weakSelf);
+        
+        [self aMethod];
+    }));
+}
+
+- (void) aMethod {
+    std::cout << "aMethod called\n";
+}
+
+@end
+
+void IOStreamDemo() {
+    
+    std::cout << @"abc" << '\n';
+    std::cout << @1 << '\n';
+    std::cout << @[@1, @2, @3] << '\n';
+    std::cout << [NSError errorWithDomain:NSURLErrorDomain code:1 userInfo:nil] << '\n';
+}
+
+void MapByStringDemo() {
+    
+    auto another_abc = [NSString stringWithFormat:@"%s", "abc"];
+    assert((NSObject*)another_abc != (NSObject*)@"abc");
+    
+    std::map<NSString *, std::string, NSStringLess> map;
+    std::unordered_map<NSString *, std::string, NSObjectHash, NSStringEqual> umap;
+    
+    map[@"abc"] = "xyz";
+    std::cout << map[another_abc] << '\n';
+    
+    
+    umap[@"abc"] = "xyz";
+    std::cout << umap[another_abc] << '\n';
+}
+
+void MapByNumberDemo() {
+    
+    auto another_25 = [NSNumber numberWithLong:25];
+    assert((NSObject*)another_25 != (NSObject*)@25);
+    
+    std::map<NSNumber *, std::string, NSNumberLess> map;
+    std::unordered_map<NSNumber *, std::string, NSObjectHash, NSNumberEqual> umap;
+    
+    map[@25] = "abc";
+    std::cout << map[another_25] << '\n';
+    
+    
+    umap[@25] = "abc";
+    std::cout << umap[another_25] << '\n';
+}
+
+void MapByObjectDemo() {
+    
+    std::unordered_map<NSURL *, int, NSObjectHash, NSObjectEqual> umap;
+    
+    auto url1 = [NSURL URLWithString:@"http://www.google.com"];
+    auto url2 = [NSURL URLWithString:@"http://www.google.com"];
+    assert((NSObject*)url1 != (NSObject*)url2);
+    
+    umap[url1] = 5;
+    std::cout << umap[url2] << '\n';
+}
+
+int main(int argc, const char * argv[]) {
+    
+    [[BlockDemo new] callBlock];
+    
+    IOStreamDemo();
+    
+    MapByStringDemo();
+    
+    MapByNumberDemo();
+    
+    MapByObjectDemo();
+    
+    return 0;
+}
